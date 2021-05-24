aac014ee834d5c18d2fe3d337c49886a9a843bca
widya <widya18ti@mahasiswa.pcr.ac.id>
2021-05-24 13:06:33 +0700

    Perbaikan

--- frontend/app/src/main/java/com/beehavesocial/mainactivity/view/RegisterActivity.kt
+++ frontend/app/src/main/java/com/beehavesocial/mainactivity/view/RegisterActivity.kt
@@ -1,12 +1,23 @@
 package com.beehavesocial.mainactivity.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.beehavesocial.mainactivity.R
+import com.beehavesocial.mainactivity.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding= ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignup.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
