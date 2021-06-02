5a3fb6e5d50967411e7c112b3fca510249c59e08
Wildan M <willnode@wellosoft.net>
2021-06-02 23:24:14 +0700

    Trainer ML done

--- research/extractor.py
+++ research/extractor.py
@@ -2,21 +2,24 @@
 # done using NLP: Sastrawi & TF-IDF
 
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-from .util.db import mydb
-from .util.stopword import filterword
+from util.db import mydb
+from util.stopword import filterword
+import re
 
 factory = StemmerFactory()
 stemmer = factory.create_stemmer()
 mycursor = mydb.cursor()
 
 sql = "SELECT id, title, content FROM wall WHERE keyword IS NULL"
+mycursor.execute(sql)
 walls = mycursor.fetchall()
-
+cleanr = re.compile('<.*?>')
 for [id, title, content] in walls:
-    stem = stemmer.stem(title + '\n' + content).split()
+    word = re.sub(cleanr, '', title + ' ' + content)
+    stem = stemmer.stem(word).split()
     stem = [filterword(x) for x in stem]
     stem = [x for x in stem if x is not None]
     keywords = ' '.join(stem)
-    mycursor.execute("UPDATE wall SET keyword %s WHERE id = %d")
+    mycursor.execute("UPDATE wall SET keyword=%s WHERE id=%s", [keywords, id])
 
 mydb.commit()
